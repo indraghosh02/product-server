16982d2b077eb8c113bb5ab3e30bc42e264bfd3d
indraghosh02 <indraghosh0802@gmail.com>
2024-08-16 01:17:28 +0600

    sorting completed

--- index.js
+++ index.js
@@ -1,18 +1,14 @@
 const express = require('express');
 const cors = require('cors');
 const { MongoClient, ServerApiVersion } = require('mongodb');
-require('dotenv').config()
+require('dotenv').config();
 const app = express();
 
 const port = process.env.PORT || 5000;
 
-//middleware
+// Middleware
 app.use(cors());
 app.use(express.json());
-
-
-
-console.log(process.env.DB_PASS);
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.obyjfl3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
@@ -22,26 +18,37 @@ const client = new MongoClient(uri, {
     version: ServerApiVersion.v1,
     strict: true,
     deprecationErrors: true,
-  }
+  },
 });
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
+    // Connect the client to the server (optional starting in v4.7)
     await client.connect();
 
     const productCollection = client.db('productDb').collection('allProducts');
 
-    app.get('/allProducts', async(req, res) =>{
-      const cursor = productCollection.find();
+    // Endpoint to get all products with sorting options
+    app.get('/allProducts', async (req, res) => {
+      const sortOption = req.query.sort;
+
+      let sortQuery = {};
+      if (sortOption === 'priceLowToHigh') {
+        sortQuery = { price: 1 };
+      } else if (sortOption === 'priceHighToLow') {
+        sortQuery = { price: -1 };
+      } else if (sortOption === 'newestFirst') {
+        sortQuery = { product_creation_date: -1 };
+      }
+
+      const cursor = productCollection.find().sort(sortQuery);
       const result = await cursor.toArray();
       res.send(result);
-    })
-
+    });
 
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    await client.db('admin').command({ ping: 1 });
+    console.log('Pinged your deployment. You successfully connected to MongoDB!');
   } finally {
     // Ensures that the client will close when you finish/error
     // await client.close();
@@ -49,12 +56,10 @@ async function run() {
 }
 run().catch(console.dir);
 
-
-
 app.get('/', (req, res) => {
-    res.send('product is running')
-})
+  res.send('Product server is running');
+});
 
 app.listen(port, () => {
-    console.log(`product is running on port ${port}`);
-})
+  console.log(`Product server is running on port ${port}`);
+});
