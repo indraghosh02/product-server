a69ea1ced35ef0ebeb16f8bb7a6403c1d2d3983a
indraghosh02 <indraghosh0802@gmail.com>
2024-08-17 17:23:28 +0600

    undo pagination

--- index.js
+++ index.js
@@ -8,7 +8,15 @@ const app = express();
 const port = process.env.PORT || 5000;
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: [
+    "http://localhost:5173",
+   "https://product-f170e.web.app",
+   "https://product-f170e.firebaseapp.com"
+
+ ],
+ credentials: true
+}));
 app.use(express.json());
 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.obyjfl3.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
@@ -25,7 +33,7 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server (optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
 
     const productCollection = client.db('productDb').collection('allProducts');
 
@@ -97,7 +105,7 @@ async function run() {
   
 
     // Send a ping to confirm a successful connection
-    await client.db('admin').command({ ping: 1 });
+    // await client.db('admin').command({ ping: 1 });
     console.log('Pinged your deployment. You successfully connected to MongoDB!');
   } finally {
     // Ensures that the client will close when you finish/error
